80892310415736613976d56715fba5c9c9d08812
Grizzly <grizzlyxyz@gmail>
2019-01-16 14:43:07 +0100

    first

---
+++ style.js
@@ -0,0 +1,6 @@
+console.log('Exo 05');
+// Exo 05
+let donnees = [14, 7 , 97, {} , "247" , 67 , 101 , true , 34 ,78 , [] ,'coding school' , 0, 11 , 32 , "italie" , 61,null,'cent',100];
+// Triez le contenu du tableau donnees en verifiant le type de chaque donnée
+// Creez 3 Tableaux : 'typeString' , 'typeNumber' , 'typeObject'.
+// Videz le tableau "donnees" en redistribuant avec l'aide de condition et d'une function prédéfinie trouvé sur internet dans les bons tableau
